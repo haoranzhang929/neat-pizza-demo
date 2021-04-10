feab6f48d0b3c4c1464541589678fb7898c66dc8
Haoran Zhang <haoranzhang@icloud.com>
2021-04-10 18:40:31 +0100

    ui update

--- client/src/components/ImagesViewer.tsx
+++ client/src/components/ImagesViewer.tsx
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Container from "@material-ui/core/Container";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import ImageList from "./ImageList";
 import ImagesViewerActions from "./ImagesViewerActions";
 import DeleteModal from "./DeleteModal";
+import Footer from "./Footer";
+import { useStyles } from "./index.style";
 
 import { Image, ItemToDelete } from "../common/model";
+import { useInterval } from "../common/useInterval";
+import Logo from "../assets/Logo.png";
 
 const fetchImages = async (store: number) => await axios.get<Image[]>(`/api/images?store=${store}`);
 
 const ImagesViewer = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+  const { container, header, logo, logoSmall } = useStyles();
+
   const [images, setImages] = useState<Image[]>();
   const [selectedStore, setSelectedStore] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
@@ -29,10 +41,15 @@ const ImagesViewer = () => {
   const geImages = () => {
     if (selectedStore !== 0) {
       setIsLoading(true);
-      fetchImages(selectedStore).then(res => {
-        setImages(res.data);
-        setIsLoading(false);
-      });
+      fetchImages(selectedStore)
+        .then(res => {
+          setImages(res.data);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.error(error);
+          setImages(undefined);
+        });
     } else {
       setImages(undefined);
     }
@@ -42,48 +59,71 @@ const ImagesViewer = () => {
     geImages();
   }, [selectedStore]);
 
+  useInterval(async () => {
+    console.log("refresh every 30 seconds");
+    let isHidden = false;
+    if (document.hidden) {
+      isHidden = true;
+    }
+    if (selectedStore !== 0 && !isLoading && !modalOpen && !isHidden) {
+      console.log("request sent");
+      setIsLoading(true);
+      const res = await fetchImages(selectedStore);
+      setImages(res.data);
+      setIsLoading(false);
+    }
+  }, 10000); // refresh every 30 seconds
+
   return (
-    <>
-      <ImagesViewerActions
-        selectedStore={selectedStore}
-        isLoading={isLoading}
-        handleRefresh={store => {
-          setIsLoading(true);
-          fetchImages(store).then(res => {
-            setImages(res.data);
-            setIsLoading(false);
-          });
-        }}
-        handleSelectStore={store => setSelectedStore(store)}
-      />
-      {images && <ImageList images={images} handleDelete={handleDelete} />}
-      {itemToDelete && (
-        <DeleteModal
-          modalOpen={modalOpen}
-          handleModalStatus={handleModalStatus}
-          itemToDelete={itemToDelete}
-          onDeleteSuccess={geImages}
-        />
-      )}
-      {isLoading && (
-        <div
-          style={{
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            position: "fixed",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            background: "rgba(220,220,220,0.2)",
-            zIndex: 100
-          }}
-        >
-          <CircularProgress style={{ alignSelf: "center" }} />
+    <div>
+      <Container component="main" maxWidth="md">
+        <div className={container}>
+          <header className={header}>
+            <img className={matches ? logoSmall : logo} src={Logo} alt="Neat Pizza Logo" />
+          </header>
+          <ImagesViewerActions
+            selectedStore={selectedStore}
+            isLoading={isLoading}
+            handleRefresh={store => {
+              setIsLoading(true);
+              fetchImages(store).then(res => {
+                setImages(res.data);
+                setIsLoading(false);
+              });
+            }}
+            handleSelectStore={store => setSelectedStore(store)}
+          />
+          {images && <ImageList images={images} handleDelete={handleDelete} />}
+          {itemToDelete && (
+            <DeleteModal
+              modalOpen={modalOpen}
+              handleModalStatus={handleModalStatus}
+              itemToDelete={itemToDelete}
+              onDeleteSuccess={geImages}
+            />
+          )}
+          {isLoading && (
+            <div
+              style={{
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
+                position: "fixed",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                background: "rgba(220,220,220,0.2)",
+                zIndex: 100
+              }}
+            >
+              <CircularProgress style={{ alignSelf: "center" }} />
+            </div>
+          )}
+          <Footer />
         </div>
-      )}
-    </>
+      </Container>
+    </div>
   );
 };
 
--- client/src/components/PageNotFound.tsx
+++ client/src/components/PageNotFound.tsx
@@ -1,28 +1,66 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
+import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import { useHistory } from "react-router-dom";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import Footer from "./Footer";
+
+import { useStyles as useStyleRoot } from "./index.style";
+
+import Logo from "../assets/Logo.png";
 
 const useStyles = makeStyles(({ spacing }) => ({
-  page404: { display: "flex", flexDirection: "column", flex: 1, justifyContent: "center" },
+  page404: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 1,
+    justifyContent: "center",
+    background: "rgba(255,255,255, 0.8)",
+    borderRadius: "25px",
+    margin: spacing(2)
+  },
   button: { width: "100px", alignSelf: "center", margin: spacing(2) }
 }));
 
 const PageNotFound = () => {
   const { page404, button } = useStyles();
+  const { root, container, header, logo, logoSmall } = useStyleRoot();
+
   const history = useHistory();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
-    <div className={page404}>
-      <Typography variant="h1" align="center">
-        404
-      </Typography>
-      <Typography variant="h3" align="center">
-        O-o-oh! Something broke.
-      </Typography>
-      <Button variant="outlined" size="small" onClick={() => history.goBack()} className={button}>
-        Go Back
-      </Button>
+    <div className={root}>
+      <Container component="main" maxWidth="sm">
+        <div className={container}>
+          <header className={header}>
+            <img className={matches ? logoSmall : logo} src={Logo} alt="Neat Pizza Logo" />
+          </header>
+          <div className={page404}>
+            <Typography variant="h1" align="center">
+              404
+            </Typography>
+            <Typography variant="h3" align="center">
+              O-o-oh! Something broke.
+            </Typography>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => history.goBack()}
+              className={button}
+            >
+              Go Back
+            </Button>
+          </div>
+          <Footer />
+        </div>
+      </Container>
     </div>
   );
 };
